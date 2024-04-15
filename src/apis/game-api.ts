7f107d30d071dd69a72d6af07396761af0d37127
ssrinath22 <ssrinath@ucsd.edu>
2024-04-15 15:20:45 -0700

    testing api endpoint

--- src/apis/game-api.ts
+++ src/apis/game-api.ts
@@ -1,13 +1,14 @@
 import axios, { AxiosResponse } from "axios";
 
-const endpoint = 'https://xlybu8czb9.execute-api.us-west-1.amazonaws.com' //deployed endpoint
-// const endpoint = 'http://localhost:8000' //local endpoint
+// const endpoint = 'https://xlybu8czb9.execute-api.us-west-1.amazonaws.com/dev' //deployed endpoint
+// const endpoint = 'https://localhost:3000/dev' //serverless offline endpoint
+const endpoint = 'http://localhost:8000' //local endpoint
 
 
 const sendUserQuery = (query: string): Promise<AxiosResponse<any, any>> => {
     const params = new URLSearchParams({ 'message': query });
 
-    return axios.get(`${endpoint}/dev/?${params.toString()}`, { withCredentials: true })
+    return axios.get(`${endpoint}/?${params.toString()}`, { withCredentials: true })
     // return axios.get(`http://localhost:8000/?${params.toString()}`) 
         .then(response => {
             console.log(response.data);
